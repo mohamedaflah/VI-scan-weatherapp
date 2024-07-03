27d2ab40dc80d0e26567f7f14a05ac3b0c92f628
Aflu <mohamedaflah186@gmail.com>
2024-07-03 14:37:06 +0530

    refactor: package.json updated

--- back-end/src/presentation/app.ts
+++ back-end/src/presentation/app.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
-app.listen(Number(process.env.SERVER_PORT) as number, () =>
+export default app.listen(Number(process.env.SERVER_PORT) as number, () =>
   console.log(` ... Server started on port ${process.env.SERVER_PORT} ... `)
 );
